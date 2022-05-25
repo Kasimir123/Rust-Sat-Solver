f9c3343e7f8939ba9c9a12d198922fb50dbe8800
nathanvercaemert <nathan.vercaemert@gmail.com>
2022-05-25 11:09:41 -0400

    trying to get the test cases

--- src/main.rs
+++ src/main.rs
@@ -9,11 +9,30 @@ pub mod var_unsat;
 use solver::Solver;
 
 // import time for stats
+use std::fs;
 use std::fs::File;
 use std::time::Instant;
 
 // main function
 fn main() {
+
+
+    let mut solver = Solver::new();
+    let paths = fs::read_dir("./benchmark-cases/20-sat/").unwrap();
+    // let paths = fs::read_dir("./benchmark-cases/50-sat/").unwrap();
+    for path in paths {
+        let f = path.as_ref().unwrap().path();
+        let benchmark_file = File::open(f).expect("failed to open benchmark file");
+        solver
+            .load_cnf(benchmark_file)
+            .expect("failed to parse benchmark file");
+        // println!("Name: {}", path.unwrap().path().display());
+        solver.solve();
+    }
+    println!("Success!");
+
+
+    
     // initialize the solver
     let mut solver = Solver::new();
 
---
+++ src/problem-finder.rs
@@ -0,0 +1,28 @@
+// quick script to find a file that isn't working
+// the last file printed before failure doesn't solve correctly
+// (if the file-name-print line isn't commented out)
+// rustc problem-finder.rs && ./problem-finder
+// (run above command from src)
+pub mod connections;
+pub mod solver;
+pub mod variable;
+pub mod conflict_set;
+pub mod var_unsat;
+use solver::Solver;
+use std::fs;
+use std::fs::File;
+fn main() {
+    let mut solver = Solver::new();
+    let paths = fs::read_dir("./../benchmark-cases/20-sat/").unwrap();
+    // let paths = fs::read_dir("./../benchmark-cases/50-sat/").unwrap();
+    for path in paths {
+        let f = path.as_ref().unwrap().path();
+        let benchmark_file = File::open(f).expect("failed to open benchmark file");
+        solver
+            .load_cnf(benchmark_file)
+            .expect("failed to parse benchmark file");
+        // println!("Name: {}", path.unwrap().path().display());
+        solver.solve();
+    }
+    println!("Success!");
+}
