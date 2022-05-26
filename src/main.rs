55b025b46173c999142da62816db371fa0ad8f62
nathanvercaemert <nathan.vercaemert@gmail.com>
2022-05-26 11:06:28 -0400

    success with 100-vars

--- src/main.rs
+++ src/main.rs
@@ -17,23 +17,24 @@ fn main() {
 
 
 
-    // // make sure everything completes (prints the failed file name if user error)
-    // // let paths = std::fs::read_dir("./benchmark-cases/20-sat/").unwrap();
+    // make sure everything completes (prints the failed file name if user error)
+    // let paths = std::fs::read_dir("./benchmark-cases/20-sat/").unwrap();
     // let paths = std::fs::read_dir("./benchmark-cases/50-sat/").unwrap();
-    // for path in paths {
-    //     let mut solver = Solver::new();
-    //     let file_path_buffer = path.as_ref().unwrap().path();
-    //     let benchmark_file = File::open(file_path_buffer).expect("failed to open benchmark file");
-    //     solver
-    //         .load_cnf(benchmark_file)
-    //         .expect("failed to parse benchmark file");
-    //     // println!("Trying: {}", path.as_ref().unwrap().path().display());
-    //     solver.solve();
-    //     if !solver.final_check() {
-    //         println!("Failed: {}", path.unwrap().path().display());
-    //     }
-    // }
-    // println!("Success!");
+    let paths = std::fs::read_dir("./benchmark-cases/100-sat/").unwrap();
+    for path in paths {
+        let mut solver = Solver::new();
+        let file_path_buffer = path.as_ref().unwrap().path();
+        let benchmark_file = File::open(file_path_buffer).expect("failed to open benchmark file");
+        solver
+            .load_cnf(benchmark_file)
+            .expect("failed to parse benchmark file");
+        // println!("Trying: {}", path.as_ref().unwrap().path().display());
+        solver.solve();
+        if !solver.final_check() {
+            println!("Failed: {}", path.unwrap().path().display());
+        }
+    }
+    println!("Success!");
 
 
     
